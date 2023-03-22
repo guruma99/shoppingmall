4af1dffc140f8579d44241fa0b51c04e6eff3dc8
깃허브아이디 <>
2023-03-22 10:13:44 +0900

    🎉 Nike shpiingmall 페이지 시작

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./component/Nav";
+import Login from "./Page/Login";
+import ProductAll from "./Page/ProductAll";
+import ProductDetail from "./Page/ProductDetail";
+
+// 1. 전체상품(ProductAll), 로그인, 상세페이지(ProductDetail)
+// 2. 로그인 전에 상세페이지 접속시, 로그인 페이지를 먼저 보여준다.
+// 3. 로그인이 이미 되어있다면, 상세페이지를 보여준다.
+// 4. 로그인 하면 로그아웃 버튼이 보이고, 반대로 로그아웃하면 로그인 버튼이 보인다.
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<ProductAll />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Page/ProductDetail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductDetail = () => {
+  return <div id="dd">ProductDetail</div>;
+};
+
+export default ProductDetail;
